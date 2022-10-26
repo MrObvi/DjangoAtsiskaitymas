3214c846475c969d7e31cdab77ec50df43340a98
Lukas <lukas.narkunas@gmail.com>
2022-10-26 09:43:44 +0300

    Translationu problema

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,9 +37,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'projektai',
     'tinymce',
     'crispy_forms',
-    'projektai',
     'django.contrib.admin',
     'sslserver',
     'social_django',
@@ -69,7 +69,7 @@ STATIC_URL = '/static/'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
